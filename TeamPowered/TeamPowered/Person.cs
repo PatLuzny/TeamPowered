e3a2747654f1c8910935f2efde3562310da3b367
Patrick <Patrick@DESKTOP-D76TCMF>
2016-07-09 15:11:00 -0400

    Add new "Role" class

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -11,5 +11,7 @@ namespace TeamPowered
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
+
+        public int Age { get; set; }
     }
 }
